d86fd3ad9110d671b9e239c98eab0337a3372885
Iker BEDECARRAX <iker.bedecarrax@ekino.com>
2023-12-14 15:35:30 +0100

    feat: add static and dynamic sinusoids

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # sinusoid-css
 
-[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/ikerbede/sinusoid-css)
+## Resources
+
+[See the video tutorial!](https://www.youtube.com/watch?v=oSzUeiYt_xU&list=PLjnstNlepBvOG299LOrvMFJ8WreCDWWd4&t=1062s)
+
+## Cosinus
+
+cos(-180) = -1 ; cos(-90) = 0 ; cos(0) = 1 ; cos(90) = 0 ; cos(180) = -1
+
+=> `0 =< cos(x) + 1 =< 2`
+=> `1 =< (cos(x) + 1) / 2 + 1 =< 2`
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello!');
+/** STEP 4: FLUID DYNAMIC SINUSOID */
+const container = document.querySelector("ul");
+const elements = document.querySelectorAll("li");
+
+const updateMouseLocalCoordinates = (ev) => {
+  const element = ev.currentTarget;
+  const elementRect = element.getBoundingClientRect();
+  const x = (ev.clientX - elementRect.left) / elementRect.width - 0.5; // -0.5 to 0.5
+  container.style.setProperty("--pX", x);
+};
+
+const resetMouseLocalCoordinates = () => {
+  container.style.setProperty("--pX", 0);
+};
+
+const iniFollowCursor = () => {
+  elements.forEach((element) => {
+    element.addEventListener("mousemove", updateMouseLocalCoordinates);
+    element.addEventListener("mouseleave", resetMouseLocalCoordinates);
+  });
+};
+
+iniFollowCursor();
